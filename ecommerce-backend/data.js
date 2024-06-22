9e35ab71cda4a1a71d7b071d604f05d2625a3cd1
lokesh-nimje <lokesh@thegeeksquad.tech>
2024-06-22 14:18:12 +0530

    added frontend and backend app

---
+++ ecommerce-backend/data.js
@@ -0,0 +1,11 @@
+const productList = [
+    { name: "Mobile", id: 1 },
+    { name: "Android", id: 2 },
+    { name: "Iphone", id: 3 },
+    { name: "Samsung", id: 4 },
+    { name: "Covers", id: 5 },
+    { name: "Oppo", id: 6 },
+    { name: "Remote", id: 7 },
+    { name: "Covers", id: 8 },
+];
+module.exports = productList
---
+++ ecommerce-backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const products = require('./data')
+const app = express()
+
+app.get('/allProducts', (req, res) => {
+    res.json(products)
+})
+app.listen(3000, () => {
+    console.log('server is listening');
+})
